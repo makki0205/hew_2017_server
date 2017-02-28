1b7b456a57d1efd65f53b5d4b170004c10b310fd
katsuramaki taiki <llxo2_5oxll@icloud.com>
2017-03-01 08:56:22 +0900

    2017年 3月 1日 水曜日 08時56分22秒 JST

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ from websocket import create_connection
 import sys
 
 #コネクションを張る
-ws = create_connection("ws://localhost:8000/raspy")
+ws = create_connection("ws://makki0250.com:8000/raspy")
 
 #メッセージを送信
 # ws.send('hello world!')
